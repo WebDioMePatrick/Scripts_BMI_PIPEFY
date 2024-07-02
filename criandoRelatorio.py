293bf4ac1e2d74346e893c02891dc735e4630f57
WebDioMePatrick <webdiomepatrick@gmail.com>
2024-07-02 17:53:58 -0300

    MicroApp Final
    micro app finalizado

--- criandoRelatorio.py
+++ criandoRelatorio.py
@@ -2,10 +2,11 @@ from docx import Document
 from docx.shared import Pt
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 from datetime import datetime
-agora = datetime.now()
-hora_atual = agora.strftime("_%Y-%m-%d_%H-%M-%S")
 
-def criar_relatorio(Tituloo,periodo,Nome,DataDoAtendimento):
+
+def criar_relatorio(Tituloo,periodo,Nome,DataDoAtendimento,valor,INICIO,FIM):
+    agora = datetime.now()
+    hora_atual = agora.strftime(F"_%Y-%m-%d_%H-%M-%S__{INICIO}__{FIM}")
     # Criando um novo documento
     documento = Document()
 
@@ -20,16 +21,18 @@ def criar_relatorio(Tituloo,periodo,Nome,DataDoAtendimento):
     # Introdução
     documento.add_heading('1. Introdução', level=2)
     documento.add_paragraph(
-        'Este relatório apresenta os resultados de vendas do período de Janeiro 2024 a Março 2024.'
+        f'Este relatório apresenta os resultados do Periodo que Foi Determinado: {periodo}.'
     )
 
     # Tabela de dados
     documento.add_heading('2. Dados Extraidos dos Cards', level=2)
+    documento.add_paragraph(f'Quantidade de Cards filtrados = {len(Nome)}')
+    
     tabela = documento.add_table(rows=1, cols=3)
     hdr_cells = tabela.rows[0].cells
     hdr_cells[0].text = 'Nome do MEI '
     hdr_cells[1].text = 'Data do Atendimento '
-    hdr_cells[2].text = 'Receita'
+    hdr_cells[2].text = 'Valor Pago R$'
 
     dados_vendas = [
         ('Janeiro', '1000', 'R$ 50.000'),
@@ -41,6 +44,7 @@ def criar_relatorio(Tituloo,periodo,Nome,DataDoAtendimento):
         row_cells = tabela.add_row().cells
         row_cells[0].text = Nome[i]
         row_cells[1].text = DataDoAtendimento[i]
+        row_cells[2].text = 'R$ '+str(valor[i])
     """ 
     for mes, vendas, receita in dados_vendas:
         row_cells = tabela.add_row().cells
@@ -55,11 +59,21 @@ def criar_relatorio(Tituloo,periodo,Nome,DataDoAtendimento):
     # documento.add_picture('caminho/para/seu/grafico.png')
 
     # Conclusão
+    somatorio = 0
+    for i in valor:
+        if i != '' and i!='campo vazio no card':
+         somatorio = somatorio+int(i)
+        
     documento.add_heading('3. Conclusão', level=2)
     documento.add_paragraph(
-        'Os resultados de vendas do período de Janeiro 2024 a Março 2024 mostram um crescimento contínuo, '
-        'com um aumento significativo em Fevereiro.'
-    )
+        f'Os resultados são referente ao periodo {periodo} ')
+    #    f'Equivale a {somatorio}.'
+    #    f'A média corresponde a {(somatorio)/(len(valor))} '
+    #)
+    documento.add_paragraph(f'Quantidade de Cards filtrados = {len(Nome)}')
+    documento.add_paragraph(f'Soma Total dos Valores filtrados = {somatorio}')
+    documento.add_paragraph(f'Média Total = {(somatorio)/(len(valor))}')
 
     # Salvando o documento
     documento.save(f'Relatorio {hora_atual}.docx')
+    return f'Arquivo Criado com Sucesso : Relatorio {hora_atual}.docx'
